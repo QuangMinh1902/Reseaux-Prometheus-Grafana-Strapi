6943db4a587f3d82d6751747816c0ff3dc8f8519
Quang Minh PHAM <quangminh1575@gmail.com>
2024-01-23 12:20:01 +0100

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,22 @@
+<!-- App.vue -->
+
 <template>
   <div id="app">
+    <h1>Mon Application</h1>
     <ArticlesList />
+    <CategoriesList />
   </div>
 </template>
 
 <script>
 import ArticlesList from "@/components/ArticlesList.vue";
+import CategoriesList from "@/components/CategoriesList.vue";
 
 export default {
   name: 'App',
   components: {
     ArticlesList,
+    CategoriesList,
   }
 }
 </script>
---
+++ frontend/src/components/CategoriesList.vue
@@ -0,0 +1,42 @@
+<!-- components/CategoriesList.vue -->
+
+<template>
+  <div>
+    <h2>Liste des Catégories</h2>
+    <ul>
+      <li v-for="category in categories" :key="category.id">
+        {{ category.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import apiClient from "@/services/api";
+
+export default {
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  mounted() {
+    this.fetchCategories();
+  },
+  methods: {
+    async fetchCategories() {
+      try {
+        const response = await apiClient.get("/categories");
+        this.categories = response.data;
+        console.log(this.categories);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des catégories", error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Vos styles CSS spécifiques au composant, si nécessaire */
+</style>
